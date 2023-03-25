5ac008b11399aa4b11e56321df373013b33483a3
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 20:49:17 +0100

    Tíz db * és tíz db o két sorban

---
+++ 003. alakzatok2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003.alakzatok2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("*o");
+
+            }
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("*o");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
