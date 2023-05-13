a101db008d56d431262737eda413b9b206ff08db
Rattlesnake1972 <grattlesnake@gmail.com>
2023-05-13 16:27:48 +0200

    alakzatok kirajzolása

--- 003. alakzatok2/Program.cs
+++ 003. alakzatok2/Program.cs
@@ -27,3 +27,12 @@ namespace _003.alakzatok2
         }
     }
 }
+
+/*Ez a kód két sorban egymás alatt kiír egy mintát a konzolra. A minta egy sorban összesen tíz karakterből áll: az összes páros helyen lévő karakter "*" karakter, az összes páratlan helyen lévő karakter "o" karakter.
+
+A program a következőket csinálja:
+
+Létrehoz egy C# konzolalkalmazást.
+Az első "for" ciklusban kiír egy sort a konzolra, amelyben egymás után jelennek meg a "*o" karakterek. Ez ötször ismétlődik.
+Azután egy sort kiír, amelyben ismét egymás után jelennek meg a "*o" karakterek. Ez is ötször ismétlődik.
+Végül a program vár a felhasználó bemenetére a "Console.ReadKey()" parancs segítségével. Amíg a felhasználó nem nyom le egy billentyűt, addig a program nem fejeződik be, és a konzolablak nyitva marad. Amint a felhasználó lenyom egy billentyűt, a program végrehajtása befejeződik.*/
